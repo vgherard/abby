957993d665ee0d4732311ea42a38d56aa286fdbe
Valerio Gherardi <valerio.gherardi@voicemod.net>
2023-01-13 16:22:38 +0100

    Minimal working example

--- R/abby.R
+++ R/abby.R
@@ -16,32 +16,59 @@
 #' @export
 abby <- function() {
 
-	# Define the user interface
 	ui <- fluidPage(
 		titlePanel("Abby"),
 
 		sidebarLayout(
 
 			sidebarPanel(
+				actionButton("compute", "Compute"),
 				sliderInput("slope", label = "Slope", min = 1, max = 10, value = 1),
-
 				selectInput(
-					"Test", label = "Hypothesis Test",
+					"test_type", label = "AB Test type",
 					choices = list(
 						"Binomial test" = "p",
 						"t-test" = "t"
 						),
 					selected = "p"),
+				selectInput(
+					"alternative", label = "Alternative",
+					choices = list(
+						"Greater" = "greater",
+						"Less" = "less",
+						"Two-Sided" = "two.sided"
+					),
+					selected = "p"),
+				numericInput(
+					"baseline",
+					label = "Baseline",
+					value = 0.1,
+					step = 0.01,
+					min = 0,
+					max = 1
+					),
+				numericInput(
+					"pct_change",
+					label = "% Change from Baseline",
+					value = 0.1,
+					step = 0.01,
+					min = 0
+					),
 
-
-				numericInput("users_batch", label = "Users per Batch", value = 1000),
+				numericInput("users_batch", label = "Users / Batch", value = 1000),
 				numericInput("min_batches", label = "Min. Batches", value = 1),
-				numericInput("max_batches", label = "Max. Batches", value = 4)
+				numericInput("max_batches", label = "Max. Batches", value = 2),
+				sliderInput("pct_traffic_a",
+										label = "% Traffic (A)", min = 0, max = 1, value = 0.5,
+										step = 0.01
+										),
+				sliderInput("pct_traffic_b",
+										label = "% Traffic (B)", min = 0, max = 1, value = 0.5,
+										step = 0.01
+										)
 				),
 
-			mainPanel(
-				plotOutput("line_plot")
-			)
+			mainPanel(plotOutput("plot"))
 
 		)
 
@@ -50,16 +77,61 @@ abby <- function() {
 	# Define the server logic
 	server <- function(input, output) {
 
-		# Create a reactive variable for the line data
-		line_data <- reactive({
-			data.frame(x = c(0, 10), y = input$slope * c(0, 10))
-		})
+			output$plot <- renderPlot({
+				.data <- tidyr::expand_grid(
+					test_type = input$test_type,
+					alternative = input$alternative,
+					baseline = input$baseline,
+					pct_change = input$pct_change,
+					users_batch = input$users_batch,
+					batches = input$min_batches:input$max_batches,
+					pct_traffic_a = input$pct_traffic_a,
+					pct_traffic_b = input$pct_traffic_b,
+					fpr = seq(from = 1, to = 999, by = 3) / 1000
+				)
+				.data$fnr <- NA_real_
 
-		# Render the line plot
-		output$line_plot <- renderPlot({
-			ggplot(line_data(), aes(x, y)) +
-				geom_line()
-		})
+				with(.data, for(i in 1:nrow(.data)) {
+					p1 <- baseline[i] * (1 + pct_change[i])
+					p2 <- baseline[i]
+					h <- pwr::ES.h(p1, p2)
+					users <- users_batch[i] * batches[i]
+					n1 <- pct_traffic_b[i] * users
+					n2 <- pct_traffic_a[i] * users
+					sig.level <-
+
+						.data$fnr[i] <<- 1 - pwr::pwr.2p2n.test(
+							h = h,
+							n1 = pct_traffic_b[i] * users,
+							n2 = pct_traffic_a[i] * users,
+							sig.level = fpr[i],
+							power = NULL,
+							alternative = alternative[i]
+						)$power
+
+				})
+
+				ggplot(.data, aes(x = fpr, y = fnr, color = pct_change)) +
+					geom_line() +
+					facet_grid(. ~ batches) +
+					geom_hline(yintercept = 0.2, linetype = "dashed") +
+					geom_vline(xintercept = 0.2, linetype = "dashed") +
+					scale_y_continuous(
+						name = "False Negative Rate",
+						breaks = seq(from = 0, to = 1, by = 0.2),
+						labels = scales::label_percent(),
+					) +
+					scale_x_continuous(
+						name = "False Positive Rate",
+						breaks = seq(from = 0, to = 1, by = 0.2),
+						labels = scales::label_percent(),
+					) +
+					guides(color = guide_legend("% Change from Baseline")) +
+					NULL
+
+			}) |>
+				bindEvent(input$compute)
+
 	}
 
 	# Run the Shiny app
