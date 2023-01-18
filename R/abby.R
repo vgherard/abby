a3cff10a060c86c203c3edc88564aeb87a48c331
Valerio Gherardi <valerio.gherardi@voicemod.net>
2023-01-18 17:25:26 +0100

    Simplify server logic using wrappers

--- R/abby.R
+++ R/abby.R
@@ -77,63 +77,13 @@ abby <- function() {
 	)
 
 	# Define the server logic
-	server <- function(input, output) {
-
+	server <- function(input, output)
+	{
 			output$plot <- renderPlot({
-				.data <- tidyr::expand_grid(
-					test_type = input$test_type,
-					alternative = input$alternative,
-					baseline = input$baseline,
-					pct_change = parse_cs_numeric_input(input$pct_change),
-					users_batch = input$users_batch,
-					batches = input$batches[1]:input$batches[2],
-					pct_traffic_a = input$pct_traffic_a,
-					pct_traffic_b = input$pct_traffic_b,
-					fpr = seq(from = 1, to = 99, by = 1) / 100
-				)
-				.data$fnr <- NA_real_
-
-				with(.data, for(i in 1:nrow(.data)) {
-					p1 <- baseline[i] * (1 + pct_change[i])
-					p2 <- baseline[i]
-					h <- pwr::ES.h(p1, p2)
-					users <- users_batch[i] * batches[i]
-					n1 <- pct_traffic_b[i] * users
-					n2 <- pct_traffic_a[i] * users
-					sig.level <-
-
-						.data$fnr[i] <<- 1 - pwr::pwr.2p2n.test(
-							h = h,
-							n1 = pct_traffic_b[i] * users,
-							n2 = pct_traffic_a[i] * users,
-							sig.level = fpr[i],
-							power = NULL,
-							alternative = alternative[i]
-						)$power
-
-				})
-
-				ggplot(.data, aes(x = fpr, y = fnr, color = as.factor(pct_change))) +
-					geom_line() +
-					facet_grid(. ~ batches) +
-					geom_hline(yintercept = 0.2, linetype = "dashed") +
-					geom_vline(xintercept = 0.2, linetype = "dashed") +
-					scale_y_continuous(
-						name = "False Negative Rate",
-						breaks = seq(from = 0, to = 1, by = 0.2),
-						labels = scales::label_percent(),
-					) +
-					scale_x_continuous(
-						name = "False Positive Rate",
-						breaks = seq(from = 0, to = 1, by = 0.2),
-						labels = scales::label_percent(),
-					) +
-					guides(color = guide_legend("% Change from Baseline")) +
-					NULL
-
+				data <- compute_power_wrap(input)
+				plot_power_grid(data)
 			}) |>
 				bindEvent(input$compute)
-
 	}
 
 	# Run the Shiny app
