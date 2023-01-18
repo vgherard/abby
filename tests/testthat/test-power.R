f5022b5da98ff8e7f0d8036cff910f494c3742e3
Valerio Gherardi <valerio.gherardi@voicemod.net>
2023-01-18 17:24:45 +0100

    Change tests argument to reflect the app's defaults

--- tests/testthat/test-power.R
+++ tests/testthat/test-power.R
@@ -3,9 +3,9 @@ test_that("compute_power throws no error with basic arguments", {
   	test_type = "p",
   	alternative = "greater",
   	baseline = 0.1,
-  	pct_change = 0.1,
+  	pct_change = c(0.05, 0.1, 0.15, 0.2),
   	users_batch = 1000,
-  	batches = c(2, 3),
+  	batches = c(1, 2),
   	pct_traffic_a = 0.5,
   	pct_traffic_b = 0.5
   ))
@@ -16,9 +16,9 @@ test_that("compute_power returns a tibble", {
 		test_type = "p",
 		alternative = "greater",
 		baseline = 0.1,
-		pct_change = 0.1,
+		pct_change = c(0.05, 0.1, 0.15, 0.2),
 		users_batch = 1000,
-		batches = c(2, 3),
+		batches = c(1, 2),
 		pct_traffic_a = 0.5,
 		pct_traffic_b = 0.5
 	)
